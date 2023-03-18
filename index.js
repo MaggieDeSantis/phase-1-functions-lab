de3de89ee7b8cd7528cf7f535ecec9b86f630b9f
MaggieDeSantis <sheisntreal@gmail.com>
2023-03-17 20:43:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance > 42) {
+        let blocks = distance % 42
+        return blocks
+      } else {
+        let blocks = 42 - distance
+        return blocks
+      }
+    }
+function distanceFromHqInFeet(distance){
+    if (distance > 42) {
+        let feet = ((distance - 42) * 264)
+        return feet
+      } else {
+        let feet = ((42 - distance) * 264)
+        return feet
+      }
+    }
+function distanceTravelledInFeet (HQ, destination){ 
+    if (HQ < destination){
+        return (destination - HQ) * 264
+    } else {
+        return (HQ - destination) * 264
+    }
+}
+function distanceTravelledInFeet (HQ, destination) {
+    if ( HQ < destination) {
+      return (destination - HQ) * 264
+    } else {
+      return (HQ - destination) * 264
+    }
+  }
+  function calculatesFarePrice(HQ, destination) {
+    const distance = distanceTravelledInFeet(HQ, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * .02
+    } else if ( distance > 2000 && distance < 2500) {
+      return 25
+    } else {
+      return 'cannot travel that far'
+    }
+  }
+  
